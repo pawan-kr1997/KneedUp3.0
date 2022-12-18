a9c67db95d0bc3a615ea44a177cbae499da554ca
Pawan Kumar <pawan.kr3012@gmail.com>
2022-12-18 09:45:31 +0530

    removing old client code

--- client/src/Components/UI/Backdrop/Backdrop.tsx
+++
@@ -1,8 +0,0 @@
-import { BackdropProps } from "../../../Utils/tscTypes";
-import "./Backdrop.css";
-
-const Backdrop: React.FC<BackdropProps> = (props) => {
-    return <div className="Backdrop" onClick={props.remove}></div>;
-};
-
-export default Backdrop;
